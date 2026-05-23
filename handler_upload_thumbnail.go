12140ee2518376aeeea47e2360ea9b4c039379f0
Zallu35 <s14.sboss@gmail.com>
2026-05-22 23:12:28 -0400

    Changed thumbnail storage to Database, deleted global map

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,45 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	fileData, headerData, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer fileData.Close()
+	mType := headerData.Header.Get("Content-Type")
+	if mType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for thumbnail", nil)
+		return
+	}
+	imageData, err := io.ReadAll(fileData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading data", err)
+		return
+	}
+	vidMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error fetching video", err)
+		return
+	}
+	if userID != vidMetadata.UserID {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	encodedImage := base64.StdEncoding.EncodeToString(imageData)
+	dataURL := "data:" + mType + ";base64," + encodedImage
+	vidMetadata.ThumbnailURL = &dataURL
+
+	err = cfg.db.UpdateVideo(vidMetadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, vidMetadata)
 }
